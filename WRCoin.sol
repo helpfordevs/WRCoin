1d06972b72c8aa1fc8c1537cafeba4aad9c36aae
helpfordevs <44855529+helpfordevs@users.noreply.github.com>
2018-11-12 14:32:56 +0000

    ERC20Basic contract updated
    from file: ERC20Basic.sol

--- WRCoin.sol
+++ WRCoin.sol
@@ -1,14 +1,17 @@
 pragma solidity ^0.4.8;
 
 /**
- * @title ERC20Basic
+ * @title ERC20Basic   ...   from file:   ERC20Basic.sol
+ * @dev Simpler version of ERC20 interface
+ * See https://github.com/ethereum/EIPs/issues/179
  */
 contract ERC20Basic {
-  uint256 public totalSupply;
-  function balanceOf(address who) public constant returns (uint256);
-  function transfer(address to, uint256 value) public returns (bool);
+  function totalSupply() public view returns (uint256);
+  function balanceOf(address _who) public view returns (uint256);
+  function transfer(address _to, uint256 _value) public returns (bool);
   event Transfer(address indexed from, address indexed to, uint256 value);
 }
+
 
 /**
  * @title ERC20 interface
